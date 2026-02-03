b39a333a78d6b8dc1ac3ab525ff872ce74633455
JOHN MAR DUMOPOL <johnmardumopol98@gmail.com>
2026-02-03 19:43:32 +0800

    Update function.js

--- 2 js/function.js
+++ 2 js/function.js
@@ -3,27 +3,41 @@ const num1Txt = document.getElementById("num1");
 const resultTxt = document.getElementById("sum");
 
 let arr = [];
-let sum = 0;
 
- for(let i=0; i<arr.length; i++ ) {
-    sum += arr[i];
+// Function to add value to array
+function addElement() {
+    let value = num1Txt.value;
+
+    if (value === "") {
+        alert("Please enter a value");
+        return;
+    }
+
+    arr.push(value);
+    displayElements();
+    num1Txt.value = "";
 }
 
+// Function to display array elements
+function displayElements() {
+    elementTxt.innerHTML = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        elementTxt.innerHTML += arr[i] + "<br>";
+    }
+}
+
+// Function to clear entries
 function clearEntries() {
+    arr = [];
+    elementTxt.innerHTML = "";
     num1Txt.value = "";
-    num2Txt.value = ""
     resultTxt.innerHTML = "";
 }
-
-function concatenate() {
-   
-}
-
-
-
 
 
 
 elementTxt.innerHTML = arr.join("<br>");
 sumTxt.innerHTML = sum;
+
 console.log(arr);
